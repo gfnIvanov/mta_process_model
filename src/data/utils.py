989f996262c47e29afec455fc1eb47f88d3ad44e
Игорь Иванов <is.ivanov@bars.group>
2023-07-24 19:57:33 +0300

    Add types

--- src/data/utils.py
+++
@@ -1,9 +0,0 @@
-def fio_comparsion(author: dict, other: list) -> bool:
-    if (
-        author["lastname"] != other[0].text
-        or author["firstname"] != other[1].text
-        or author["surname"] != other[2].text
-    ):
-        return False
-    else:
-        return True
